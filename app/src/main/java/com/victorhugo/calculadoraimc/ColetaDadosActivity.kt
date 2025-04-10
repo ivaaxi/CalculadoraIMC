efb9379f49a90a86bf0dde10d355b281d579a63c
ivaaxi <vitcheikta@hotmail.com>
2025-04-10 15:10:01 -0300

    Primeiro commit: Criação de app para calcular imc e indicar peso ideal

---
+++ app/src/main/java/com/victorhugo/calculadoraimc/ColetaDadosActivity.kt
@@ -0,0 +1,113 @@
+package com.victorhugo.calculadoraimc
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.progressindicator.CircularProgressIndicator
+
+const val MILLISECONDS_LOADING_BAR = 2000L
+
+class ColetaDadosActivity : AppCompatActivity() {
+
+    private val edtPeso: EditText by lazy { findViewById(R.id.edtPeso) }
+    private val edtAltura: EditText by lazy { findViewById(R.id.edtAltura) }
+    private val btnCalcularImc: Button by lazy { findViewById(R.id.btnCalcularImc) }
+    private val pbLoading: CircularProgressIndicator by lazy { findViewById(R.id.progressIndicator) }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.coleta_dados_activity)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        setupButtonListener()
+    }
+
+    private fun setupButtonListener() {
+        this.btnCalcularImc.setOnClickListener {
+            if (!validateFields()) return@setOnClickListener
+
+            val pesoTexto = edtPeso.text.toString().replace(",", ".").trim()
+            val alturaTexto = edtAltura.text.toString().trim()
+
+            val peso = normalizaPeso(pesoTexto)
+            val altura = normalizarAltura(alturaTexto)
+
+            if (peso == null) {
+                edtPeso.error = "Peso inválido"
+                return@setOnClickListener
+            }
+
+            if (altura == null) {
+                edtAltura.error = "Altura inválida"
+                return@setOnClickListener
+            }
+
+            hideKeyBoard()
+            val intent = Intent(this, ResultadoActivity::class.java)
+            intent.putExtra("peso", peso.toString())
+            intent.putExtra("altura", altura.toString())
+            setupProgressBar(intent)
+        }
+    }
+
+    private fun hideKeyBoard(){
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+    }
+
+    private fun normalizarAltura(input: String): Float? {
+        val alturaLimpa = input.replace(",", ".").trim()
+
+        return when {
+            alturaLimpa.contains('.') -> {
+                alturaLimpa.toFloatOrNull()
+            }
+            alturaLimpa.length == 3 -> {
+                // Ex: 190 -> 1.90
+                val alturaFormatada = alturaLimpa[0] + "." + alturaLimpa.substring(1)
+                alturaFormatada.toFloatOrNull()
+            }
+            else -> {
+                alturaLimpa.toFloatOrNull()
+            }
+        }
+    }
+
+    private fun normalizaPeso(input: String) : Float? {
+        val pesoLimpo = input.replace(",", ".").trim()
+        return pesoLimpo.toFloatOrNull()
+    }
+
+    private fun setupProgressBar(intent: Intent) {
+        this.btnCalcularImc.isEnabled = false
+        this.pbLoading.visibility = View.VISIBLE
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.pbLoading.visibility = View.GONE
+            this.btnCalcularImc.isEnabled = true
+            startActivity(intent)
+        }, MILLISECONDS_LOADING_BAR)
+    }
+
+    private fun validateFields(): Boolean {
+        val peso = this.edtPeso.text.toString()
+        val altura = this.edtAltura.text.toString()
+        if (peso.isEmpty()) {
+            this.edtPeso.error = "Campo obrigatório"
+            return false
+        }
+        if (altura.isEmpty()) {
+            this.edtAltura.error = "Campo obrigatório"
+            return false
+        }
+        return true
+    }
+}
---
+++ app/src/main/java/com/victorhugo/calculadoraimc/ResultadoActivity.kt
@@ -0,0 +1,94 @@
+package com.victorhugo.calculadoraimc
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultadoActivity : AppCompatActivity() {
+    private val txtPesoInformado: TextView by lazy { findViewById(R.id.txtPesoInformado) }
+    private val txtAlturaInformada: TextView by lazy { findViewById(R.id.txtAlturaInformada) }
+    private val txtResultado: TextView by lazy { findViewById(R.id.txtResultado) }
+    private val txtRecomendacao: TextView by lazy { findViewById(R.id.txtRecomendacao) }
+    private val btnRecalcular: Button by lazy { findViewById(R.id.btnRecalcular) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resultado_activity)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setupTextViews()
+        setupListeners()
+    }
+    private fun setupListeners() {
+        btnRecalcular.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun setupTextViews() {
+        val peso = intent.getStringExtra("peso")?.toDoubleOrNull() ?: 0.0
+        val altura = intent.getStringExtra("altura")?.toDoubleOrNull() ?: 0.0
+        val resultadoImc = calcularIMC(peso, altura)
+        calcularPesoIdeal(altura)
+        val precisaPerder = precisaPerder(peso, altura)
+
+
+        txtPesoInformado.text =
+            getString(
+                R.string.txtPesoInformado,
+                peso
+            )
+        txtAlturaInformada.text =
+            getString(
+                R.string.txtAlturaInformada,
+                altura
+            )
+        txtResultado.text =
+            getString(
+                R.string.txtResultado,
+                faixaIMC(resultadoImc)
+            )
+        txtRecomendacao.text = if (precisaPerder > 0) {
+            getString(
+                R.string.txtRecomendacao,
+                precisaPerder
+            )
+        } else {
+            "Você está dentro do peso considerado saudável!"
+        }
+
+
+    }
+
+    private fun calcularIMC(peso: Double, altura: Double): Double {
+        return peso / (altura * altura)
+    }
+
+    private fun faixaIMC(resultadoImc: Double): String {
+        return when {
+            resultadoImc < 18.5 -> "Abaixo do peso"
+            resultadoImc < 24.9 -> "Peso normal"
+            resultadoImc < 29.9 -> "Sobrepeso"
+            resultadoImc < 34.9 -> "Obesidade grau 1"
+            resultadoImc < 39.9 -> "Obesidade grau 2"
+            else -> "Obesidade grau 3"
+        }
+    }
+
+    private fun pesoMaximoNormal(altura: Double): Double {
+        return 24.9 * (altura * altura)
+    }
+
+    private fun precisaPerder(peso: Double, altura: Double): Double {
+        val pesoLimite = pesoMaximoNormal(altura)
+        return peso - pesoLimite
+    }
+
+    private fun calcularPesoIdeal(altura: Double): Double {
+        return 72.7 * altura - 58
+    }
+
+}
